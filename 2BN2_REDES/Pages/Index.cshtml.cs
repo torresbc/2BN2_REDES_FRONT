05585ce67388563f64dcef154cec6c4854af10b7
Wesley Rogério S Campos <56031533+WesleyRSC@users.noreply.github.com>
2022-05-22 23:27:04 -0300

    Adicionar arquivos de projeto.

---
+++ 2BN2_REDES/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2BN2_REDES.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> ShowContainers  ()
+        {
+            Console.WriteLine("Conteiner aqui");
+            return NotFound("Não encontrei container");
+        }
+    }
+}
